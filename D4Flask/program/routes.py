0a57a3b21e453bc368b609e928d7e98f3ca0e6ab
Michael Esch <eschmichael@yandex.com>
2019-10-03 16:30:47 -0400

    stylized nav and about view added

--- D4Flask/program/routes.py
+++ D4Flask/program/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from program import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
